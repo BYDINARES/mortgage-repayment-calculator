1fa0e25059c66a3e71093fe740bb54a6716bbf4e
David Gutierrez <dagd0918@gmail.com>
2025-03-07 07:10:42 -0300

    Finiching the error alert  system

--- script.js
+++ script.js
@@ -24,51 +24,11 @@ const emptyResults = document.querySelector(".empty-results");
 const completedResults = document.querySelector(".completed-results");
 
 //========================== Function ================================//
-function amountToPay() {
-  const mortgageAmountValue = +mortgageAmount.value;
-  const mortgageTermValue = +mortgageTerm.value;
-  const interestRateValue = +interestRate.value;
 
-  mortgageAmount.addEventListener("input", isTheProcessCorrect);
-
-  //The two option inputs
-  let mortgageType = document.querySelector(
-    'input[name="mortgageType"]:checked'
-  ).value;
-
-  if (mortgageType === "repayment") {
-    //The rate
-    const monthlyInterestRate = interestRateValue / 100 / 12;
-    //The number of months to pay for
-    const monthsTerm = mortgageTermValue * 12;
-    //The bottom section of the fraction (operation)
-    const denominator = 1 - Math.pow(1 + monthlyInterestRate, -monthsTerm);
-
-    const monthlyFractionResult =
-      (mortgageAmountValue * monthlyInterestRate) / denominator;
-
-    const totalRepaymentAmount = monthlyFractionResult * monthsTerm;
-    //- See form validation messages if any field is incomplete
-
-    monthlyRepaymnets.textContent = Math.round(monthlyFractionResult);
-    totalPayment.textContent = Math.round(totalRepaymentAmount);
-  } else if (mortgageType === "interest-only") {
-    const monthlyInterestToPay =
-      (mortgageAmountValue * (interestRateValue / 100)) / 12;
-    monthlyRepaymnets.textContent = Math.round(monthlyInterestToPay);
-
-    const totalInterestsToPay = monthlyInterestToPay * 12 * mortgageTermValue;
-    totalPayment.textContent = Math.round(
-      totalInterestsToPay + mortgageAmountValue
-    );
-  } else {
-    monthlyRepaymnets.textContent = "";
-    totalPayment.textContent = "";
-  }
-}
-//========= The variable which determines if the function is right or not ============
+// The variable which determines if the function is right or not
 let isValid = false;
 
+//============= The function which determines if the process is correct ============
 function isTheProcessCorrect(input) {
   // Remove previous error messages
   const existingError =
@@ -86,15 +46,16 @@ function isTheProcessCorrect(input) {
       `<p class="error-message" style="color: red; font-size: 1rem;">You typed nothing</p>`
     );
     input.parentNode.classList.add("error");
+    isValid = false;
   } else if (isNaN(input.value)) {
     input.parentNode.parentNode.insertAdjacentHTML(
       "beforeend",
       `<p class="error-message" style="color: red; font-size: 1rem;">You typed wrong</p>`
     );
     input.parentNode.classList.add("error");
+    isValid = false;
   } else {
     input.parentNode.classList.remove("error");
-    button.addEventListener("click", amountToPay);
     isValid = true;
   }
 }
@@ -105,6 +66,57 @@ mortgageAmount.addEventListener("input", () =>
 mortgageTerm.addEventListener("input", () => isTheProcessCorrect(mortgageTerm));
 interestRate.addEventListener("input", () => isTheProcessCorrect(interestRate));
 
+function amountToPay() {
+  const mortgageAmountValue = +mortgageAmount.value;
+  const mortgageTermValue = +mortgageTerm.value;
+  const interestRateValue = +interestRate.value;
+
+  mortgageAmount.addEventListener("input", isTheProcessCorrect);
+
+  //The two option inputs
+  let mortgageType = document.querySelector(
+    'input[name="mortgageType"]:checked'
+  ).value;
+
+  if (isValid) {
+    if (mortgageType === "repayment") {
+      //The rate
+      const monthlyInterestRate = interestRateValue / 100 / 12;
+      //The number of months to pay for
+      const monthsTerm = mortgageTermValue * 12;
+      //The bottom section of the fraction (operation)
+      const denominator = 1 - Math.pow(1 + monthlyInterestRate, -monthsTerm);
+
+      const monthlyFractionResult =
+        (mortgageAmountValue * monthlyInterestRate) / denominator;
+
+      const totalRepaymentAmount = monthlyFractionResult * monthsTerm;
+      //- See form validation messages if any field is incomplete
+
+      monthlyRepaymnets.textContent = Math.round(monthlyFractionResult);
+      totalPayment.textContent = Math.round(totalRepaymentAmount);
+    } else if (mortgageType === "interest-only") {
+      const monthlyInterestToPay =
+        (mortgageAmountValue * (interestRateValue / 100)) / 12;
+      monthlyRepaymnets.textContent = Math.round(monthlyInterestToPay);
+
+      const totalInterestsToPay = monthlyInterestToPay * 12 * mortgageTermValue;
+      totalPayment.textContent = Math.round(
+        totalInterestsToPay + mortgageAmountValue
+      );
+    }
+    emptyResults.style.zIndex = "0";
+    completedResults.style.zIndex = "1";
+  } else {
+    monthlyRepaymnets.textContent = "";
+    totalPayment.textContent = "";
+    emptyResults.style.zIndex = "1";
+    completedResults.style.zIndex = "0";
+  }
+}
+button.addEventListener("click", amountToPay);
+
+//========= The button ====================
 function clearAll() {
   mortgageAmount.value = "";
   mortgageTerm.value = "";
@@ -123,12 +135,16 @@ function clearAll() {
     .querySelectorAll(".error-message")
     .forEach((error) => error.remove());
 
+  //For the two option section
+  document
+    .querySelectorAll('input[name="mortgageType"]')
+    .forEach((radio) => (radio.checked = false));
+
   // Reset input borders
   const allInputContainers = document.querySelectorAll(".iput-boxes");
   allInputContainers.classList.remove();
 }
 
-//========= The button ====================
 clearAllButton.addEventListener("click", clearAll);
 if (isValid) {
   emptyResults.style.zIndex = "0";
